aa6f3247017be956452f28192515e2eef335814b
李江 <lijiang@xiaoshouyi.com>
2018-12-29 18:46:15 +0800

    增加数据管理代码

---
+++ MoneyManager/Business/DataBase/MoneyProtocal.h
@@ -0,0 +1,13 @@
+//
+//  MoneyProtocal.h
+//  MoneyManager
+//
+//  Created by RiverLi on 2018/12/29.
+//  Copyright © 2018年 RiverLi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MoneyProtocal <NSObject>
+@property(nonatomic, copy) NSString *name;
+@end
--- MoneyManager/Business/DataBase/RLDataBaseUtil.m
+++ MoneyManager/Business/DataBase/RLDataBaseUtil.m
@@ -30,6 +30,10 @@
     return [self sharedInstance];
 }
 
+- (NSURL*)applicationDocumentsDirectory {
+    return[[[NSFileManager defaultManager]URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask]lastObject];
+}
+
 -(NSManagedObjectContext *)objectContext {
     if (!_objectContext) {
         _objectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
@@ -39,16 +43,26 @@
 
 - (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
     if (!_persistentStoreCoordinator) {
-        
+        _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:self.objectModel];
+        NSError *error = nil;
+        NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"money.sqlite"];
+        if ([_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+            NSLog(@"创建数据库失败:%@", error);
+        }
     }
     return _persistentStoreCoordinator;
 }
 
 - (NSManagedObjectModel *)objectModel {
     if (!_objectModel) {
-        
+        NSURL *url = [[NSBundle mainBundle] URLForResource:@"MoneyDataBase" withExtension:@"momd"];
+        _objectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:url];
     }
     return _objectModel;
+}
+
+- (void)addMoney:()money {
+    
 }
 
 @end
